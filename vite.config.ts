f20177db0e91af65b484166d3a0bd283eebb67ae
kumarrajdevops <kumarrajdevops@gmail.com>
2025-02-22 16:08:36 +0530

    configured-1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/filehub-frontend/",
   server: {
     hmr: {
       overlay: false, // 🔹 Disables Vite's WebSocket overlay errors
