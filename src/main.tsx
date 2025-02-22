76593f33c68837ecc46f8e160d35b5ac7791b868
kumarrajdevops <kumarrajdevops@gmail.com>
2025-02-22 16:15:49 +0530

    GitHub Pages  required basename.

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/filehub-frontend">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
